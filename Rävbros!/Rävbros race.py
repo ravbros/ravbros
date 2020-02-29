7312b2faded8336153fd4e60093fa62b5afe3890
Räv Bros <61623367+ravbros@users.noreply.github.com>
2020-02-29 15:13:34 +0100

    spelet kan starta om
    NAJS

--- Rävbros!/Rävbros race.py
+++ Rävbros!/Rävbros race.py
@@ -9,42 +9,52 @@ räv.y=550
 apelsin.x=750
 apelsin.y=550
 HOPP=0
+speletkör=False
 
 def draw():
-    
-    
-
     screen.clear
     screen.blit("bakgrund",(0,0))
-    räv.draw()
-    apelsin.draw()
-    
-
+    if speletkör:
+        räv.draw()
+        apelsin.draw()
+    else:
+        screen.draw.text("TRYCK PÅ MELLANSLAG FÖR EN ÖVERRASKNING",
+                         center=(400,500),fontsize=30)
+def börja():
+    global räv,apelsin, speletkör,HOPP
+    speletkör=True
+    räv.x=50
+    räv.y=550
+    apelsin.x=750
+    apelsin.y=550
+    HOPP=0
     
     
 def update():
-    global räv,apelsin, spelet_slut,HOPP
-    apelsin.x=apelsin.x-5
-    if apelsin.x<0:
-        apelsin.x=800
-    if räv.y<550:
-        räv.y=räv.y+5
-    if räv.y==550 and keyboard.space:
-        HOPP=30
-    if HOPP>0:
-        räv.y=räv.y-15
-        HOPP=HOPP-1
-        
-
-    döden=apelsin.colliderect(räv)
-    if döden:
-        sounds.död.play()
-        spelet_slut=True
+    global räv,apelsin, speletkör,HOPP
+    if speletkör:
+        apelsin.x=apelsin.x-5
+        if apelsin.x<0:
+            apelsin.x=800
+        if räv.y<550:
+            räv.y=räv.y+5
+        if räv.y==550 and keyboard.up:
+            HOPP=30
+        if HOPP>0:
+            räv.y=räv.y-15
+            HOPP=HOPP-1
             
+
+        döden=apelsin.colliderect(räv)
+        if döden:
+            sounds.död.play()
+            speletkör=False
+        räv.x=max(10,min (räv.x, BREDD-30))
+        räv.y=max(10,min (räv.y, HOJD-30))
+    else:
+        if keyboard.space:
+            börja()
         
-    
-    räv.x=max(10,min (räv.x, BREDD-30))
-    räv.y=max(10,min (räv.y, HOJD-30))
 
 pgzrun.go()   
     
