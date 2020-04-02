562c899a77620bcd4954a281507ccb9755ce16eb
Räv Bros <61623367+ravbros@users.noreply.github.com>
2020-04-02 19:41:47 +0200

    Revert "j"
    This reverts commit 7fa8cf17442c5a2a2c1124cc07c4f8dfd82b18f8.

--- RÄVRIS/RÄVRIS.py
+++ RÄVRIS/RÄVRIS.py
@@ -3,19 +3,22 @@ from random import randint
 
 HOJD=600
 BREDD=800
-rosa=Actor("apple")
+rosa=Actor("spak")
 Bitar=[]
-
+endgame=False
 def draw():
+    global endgame
     screen.clear
     screen.fill("blue")
     for bit in Bitar:
         bit.draw()
- 
+    if endgame==True:
+        screen.blit("endgame",(0,0))       
+         
 
 def ny_bit():
     global rosa
-    rosa=Actor("apple")
+    rosa=Actor("spak")
     rosa.x=randint(200,600)
     rosa.y=20
     Bitar.append(rosa)
@@ -23,24 +26,41 @@ def ny_bit():
 
 
 
-def flytta_rosa():                
-    if rosa.y<500:
-        rosa.y=rosa.y+20
-    else:
-        ny_bit()
-            
+def flytta_rosa():
+    global endgame
+    if not endgame:
+        if rosa.y<510:
+            rosa.y=rosa.y+20
+        else:
+            ny_bit()
+                
 
 def update():
-    if keyboard.right:
-        rosa.x=rosa.x+10
-        screen.fill("blue")
-    if keyboard.left:
-        rosa.x=rosa.x-10
-        screen.fill("black")
-    
-
+    global endgame
+    if endgame==False:
+            krocka=False
+            if keyboard.right:
+                rosa.x=rosa.x+10
+            if keyboard.left:
+                rosa.x=rosa.x-10
+            if keyboard.down:
+                rosa.y=rosa.y+10
+            for bit in Bitar:
+                if bit==rosa:
+                    continue
+                krockabit=rosa.colliderect(bit)
+                if krockabit:
+                    krocka=True
+            if krocka:
+                if rosa.y<100:
+                    endgame=True
+                else:
+                    ny_bit()
+        
+        
 ny_bit()  
 clock.schedule_interval(flytta_rosa,1)
+
 
 
 
