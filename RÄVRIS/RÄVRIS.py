59e4ce8d406d566bece37296ef6bcff048b0a7f5
Räv Bros <61623367+ravbros@users.noreply.github.com>
2020-04-02 19:17:02 +0200

    very ssupri nojs
    a very good game with the good game pants on

--- RÄVRIS/RÄVRIS.py
+++ RÄVRIS/RÄVRIS.py
@@ -3,19 +3,22 @@ from random import randint
 
 HOJD=600
 BREDD=800
-rosa=Actor("apple")
+rosa=Actor("spak")
 Bitar=[]
-
+endgame=False
 def draw():
+    global endgame
     screen.clear
     screen.fill("blue")
     for bit in Bitar:
         bit.draw()
- 
+    if endgame==True:
+        pass       
+         
 
 def ny_bit():
     global rosa
-    rosa=Actor("apple")
+    rosa=Actor("spak")
     rosa.x=randint(200,600)
     rosa.y=20
     Bitar.append(rosa)
@@ -23,27 +26,37 @@ def ny_bit():
 
 
 
-def flytta_rosa():                
-    if rosa.y<550:
-        rosa.y=rosa.y+20
-    else:
-        ny_bit()
-            
+def flytta_rosa():
+    global endgame
+    if not endgame:
+        if rosa.y<510:
+            rosa.y=rosa.y+20
+        else:
+            ny_bit()
+                
 
 def update():
-    krocka=False
-    if keyboard.right:
-        rosa.x=rosa.x+10
-    if keyboard.left:
-        rosa.x=rosa.x-10
-    for bit in Bitar:
-        if bit==rosa:
-            continue
-        krockabit=rosa.colliderect(bit)
-        if krockabit:
-            krocka=True
-    if krocka:
-        ny_bit()
+    global endgame
+    if endgame==False:
+            krocka=False
+            if keyboard.right:
+                rosa.x=rosa.x+10
+            if keyboard.left:
+                rosa.x=rosa.x-10
+            if keyboard.down:
+                rosa.y=rosa.y+10
+            for bit in Bitar:
+                if bit==rosa:
+                    continue
+                krockabit=rosa.colliderect(bit)
+                if krockabit:
+                    krocka=True
+            if krocka:
+                if rosa.y<100:
+                    endgame=True
+                else:
+                    ny_bit()
+        
         
 ny_bit()  
 clock.schedule_interval(flytta_rosa,1)
