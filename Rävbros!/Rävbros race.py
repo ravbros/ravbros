04ee94dbfba2bdac8ccd3a6b178f1fae8852ce71
Räv Bros <61623367+ravbros@users.noreply.github.com>
2020-04-16 19:26:59 +0200

    NOJS

--- Rävbros!/Rävbros race.py
+++ Rävbros!/Rävbros race.py
@@ -4,12 +4,16 @@ HOJD=600
 BREDD=800
 räv=Actor("fox")
 apelsin=Actor("monster_left")
+p=Actor("påle")
 räv.x=50
 räv.y=550
 apelsin.x=750
 apelsin.y=570
+p.x=750
+p.y=300
 HOPP=0
 speletkör=False
+utekott=0
 
 def draw():
     screen.clear
@@ -22,6 +26,8 @@ def draw():
     else:
         screen.draw.text("TRYCK PÅ MELLANSLAG FÖR EN ÖVERRASKNING",
                          center=(400,500),fontsize=30)
+    if pp:
+        p.draw()
 def börja():
     global räv,apelsin, speletkör,HOPP
     speletkör=True
@@ -35,28 +41,33 @@ def börja():
 def update():
     global räv,apelsin, speletkör,HOPP
     if speletkör:
-        apelsin.x=apelsin.x-5
-        if apelsin.x<0:
-            apelsin.x=800
-        if räv.y<550:
-            räv.y=räv.y+5
-        if räv.y==550 and keyboard.up:
-            HOPP=30
-        if HOPP>0:
-            räv.y=räv.y-15
-            HOPP=HOPP-1
-            
-
-        döden=apelsin.colliderect(räv)
-        if döden:
-            sounds.död.play()
-            speletkör=False
-        räv.x=max(10,min (räv.x, BREDD-30))
-        räv.y=max(10,min (räv.y, HOJD-30))
+        if utekott<10:
+            apelsin.x=apelsin.x-5
+            if apelsin.x<0:
+                apelsin.x=800
+                utekott=utekott+1
+            if räv.y<550:
+                räv.y=räv.y+5
+            if räv.y==550 and keyboard.up:
+                HOPP=30
+            if HOPP>0:
+                räv.y=räv.y-15
+                HOPP=HOPP-1
     else:
-        if keyboard.space:
-            börja()
+        pp=True
         
+                
+
+                    döden=apelsin.colliderect(räv)
+                    if döden:
+                        sounds.död.play()
+                        speletkör=False
+                    räv.x=max(10,min (räv.x, BREDD-30))
+                    räv.y=max(10,min (räv.y, HOJD-30))
+                else:
+                    if keyboard.space:
+                        börja()
+                
 
 pgzrun.go()   
     
